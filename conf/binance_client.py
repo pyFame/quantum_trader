befae997cad10d52893d5098e8c0b29693bff762
Hiro <pythoneerHiro@gmail.com>
2023-04-13 11:10:51 +0530

    :recycle: binance_client.py
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- conf/binance_client.py
+++ conf/binance_client.py
@@ -1,7 +1,7 @@
+from binance import Client, AsyncClient
 
-from binance import Client, AsyncClient, BinanceSocketManager
 
-def gen_client(testnet=True): #TODO: move to utils
+def gen_client(testnet=True):  # TODO: move to utils
     if testnet:
         api_key = "61f1b8ba72db33f5685e2d280b49fd4dcdd43d0419112e325e27ed6c2805b76b"
         api_secret = '3a04aa40af1039286e92d1e1e59ec323fed94fbc934da1ce46d0348c3a6f68ac'
@@ -10,8 +10,8 @@ def gen_client(testnet=True): #TODO: move to utils
 
     async_client = AsyncClient(api_key, api_secret, testnet=testnet)  # ,tld='us')
 
-
     return client, async_client
 
+
 testnet = True
-client,async_client = gen_client(testnet)
+client, async_client = gen_client(testnet)
