6db30cb32e64f95f7f1177cf49b0f0d04a444d36
Hiro <pythoneerHiro@gmail.com>
2023-04-16 13:56:00 +0530

    :zap: keepAlive
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-from dask import compute, delayed
-
 from order_consumer.main import main as main_order_consumer
 from strategy.main import main as main_start_strategy
 from trader.main import main as main_trader
+from utils.thread import keepAlive
 
 
-@delayed
+@keepAlive(daemon=False)
 def start_strategy():
     main_start_strategy()
 
 
-@delayed
+# @keepAlive(daemon=False)
 def start_order_consumer():
     main_order_consumer()
 
 
-@delayed
+# @undead
+@keepAlive(daemon=False)
 def start_trader():
     main_trader()
 
 
 def main():
-    compute(*[
-        start_strategy(),
-        start_order_consumer(),
-        start_trader(),
-    ], scheduler="threads")  # make root logger safe
+    start_strategy()
+    start_trader()
+    start_order_consumer()
+    # time.sleep()
+
+    # compute(*[
+    #     start_strategy(),
+    #     start_order_consumer(),
+    #     start_trader(),
+    # ], scheduler="threads")  # FIXME make root logger safe
 
 
 if __name__ == "__main__":
