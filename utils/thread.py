fe7651c6fa8c679b9070772205f699488bf28bd3
Hiro <pythoneerHiro@gmail.com>
2023-04-16 13:53:31 +0530

    :bug: undead, keepAlive
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- utils/thread.py
+++ utils/thread.py
@@ -1,18 +1,24 @@
-from threading import Thread, enumerate as threads, current_thread, Lock
 from multiprocessing import Process
+from threading import Thread
 
 
-def keepAlive(fc, daemon=True):
-    def inner(*args):
-        t = Thread(name=fc.__name__, target=fc, args=args, daemon=daemon)
-        t.start()
+def keepAlive(daemon=True):
+    def outter(fc):
+        def inner(*args):
+            t = Thread(name=fc.__name__, target=fc, args=args, daemon=daemon)
+            t.start()
 
-    return inner
+        return inner
+
+    return outter
 
 
-def undead(fc, daemon=True):
-    def inner(*args):
-        p = Process(name=fc.__name__, target=fc, args=args, daemon=daemon)
-        p.start()
+def undead(daemon=True):
+    def outter(fc):
+        def inner(*args):
+            p = Process(name=fc.__name__, target=fc, args=args, daemon=daemon)
+            p.start()
 
-    return inner
+        return inner
+
+    return outter
